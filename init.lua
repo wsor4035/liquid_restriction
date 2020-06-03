a65eb891e7a819874121b98d35f7018a98ba8e62
wsor <wsor@gmail.com>
2020-06-03 02:45:36 -0400

    initial commit

---
+++ init.lua
@@ -0,0 +1,44 @@
+--registering priv
+minetest.register_privilege("spill", {description = "Able to use all liquids.", give_to_singleplayer=false})
+
+--list of all liquid nodes, feel free to add your own
+local liquid_list = {
+    "default:water_source",
+    "default:river_water_source",
+	"default:lava_source",
+}
+
+--list of liquid nodes in buckets, feel free to add your own
+local liquid_bucket_list = {
+    "bucket:bucket_lava",
+    "bucket:bucket_water",
+    "bucket:bucket_river_water",
+}
+
+--reads list, overrides nodes, adding priv check
+for liquidcount = 1, #liquid_list do
+	minetest.override_item(liquid_list[liquidcount], {
+		on_place = function(itemstack, placer, pointed_thing)
+			if not minetest.check_player_privs(placer:get_player_name(), {spill = true}) then
+				minetest.chat_send_player(placer:get_player_name(), "Spill priv required to use this node")
+				return
+            else
+                return minetest.item_place(itemstack, placer, pointed_thing)
+            end
+		end	
+	})
+end
+
+--reads list, overrides nodes, adding priv check
+for liquidbucketcount = 1, #liquid_bucket_list do
+    minetest.override_item(liquid_bucket_list[liquidbucketcount], {
+        on_place = function(itemstack, placer, pointed_thing)
+            if not minetest.check_player_privs(placer:get_player_name(), {spill = true}) then
+                minetest.chat_send_player(placer:get_player_name(), "Spill priv required to use this node")
+                return itemstack
+            else
+                return minetest.item_place(itemstack, placer, pointed_thing)
+            end
+        end
+    })
+end
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Minetest Liquid_Restriction Mod
+
+restricts players from placing liquids via the node or bucket unless they have a the spill priv  
+
+## About
+
+born out of me not being able to find a good public spill mod. mod is ment for creative worlds. licensed under MIT.
+
+## Links
+
+* [Github](not avaible yet)
+* [Contentdb](not avaible yet)
+* [forums](not avaible yet)
+
+## Setup
+
+download mod, make sure its named liquid_restriction
+
+## FAQ
+
+__buckets don't work(place liquid) when used.__   
+just use the node form of it. use default:water_source instead of bucket:bucket_water
+
+__how do I use this mod for random liquid__  
+simple, add random liquids mod to the depends.txt file,
+and the modname:node_name to to the liquid_list.
+if it has a bucket form, add its bucket:node_name to 
+liquid_bucket_list
